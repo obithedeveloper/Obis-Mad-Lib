9da3ea6a84f56d7a02746308e97853f40e3af940
Obinna <inlandobinna@gmail.com>
2020-02-06 11:15:25 -0500

    Initial Commit

---
+++ Obis Mad Lib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Obis Mad Lib
+//
+//  Created by Obinna Ezekwesili on 2/6/20.
+//  Copyright © 2020 Obinna Ezekwesili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Obis Mad LibTests/Obis_Mad_LibTests.swift
@@ -0,0 +1,34 @@
+//
+//  Obis_Mad_LibTests.swift
+//  Obis Mad LibTests
+//
+//  Created by Obinna Ezekwesili on 2/6/20.
+//  Copyright © 2020 Obinna Ezekwesili. All rights reserved.
+//
+
+import XCTest
+@testable import Obis_Mad_Lib
+
+class Obis_Mad_LibTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
